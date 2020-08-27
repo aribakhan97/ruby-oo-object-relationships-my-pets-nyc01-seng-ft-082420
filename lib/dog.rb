169da49520ce5b51f83b26a437277ccd0cba63a8
aribakhan97 <akhan41@binghamton.edu>
2020-08-27 16:13:20 -0400

    class methods and associations for dog, cat and owner

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
-end
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @mood = "nervous"
+    @owner = owner
+    @name = name 
+    @@all << self 
+  end 
+
+      def self.all 
+      @@all 
+    end 
+end 
